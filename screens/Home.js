4bed82f3939340aca2ba69fa29a93979a3069dda
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-16 21:50:47 +0530

    create screens and change APP.js

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Home(props) {
+  return (
+    <View>
+      <Text> HOME</Text>
+    </View>
+  );
+}
+
+export default Home;
---
+++ screens/Restaurant.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Restaurant(props) {
+  return (
+    <View>
+      <Text> Restaurant</Text>
+    </View>
+  );
+}
+
+export default Restaurant;
