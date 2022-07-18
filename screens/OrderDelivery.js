876e70675160a987f27ed4ec2259b40e2327cd08
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-18 23:39:45 +0530

    Start Order Delivery Page

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,12 +1,103 @@
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {COLORS, FONTS, SIZES, icons, images} from '../constants';
 import React from 'react';
-import {View, Text} from 'react-native';
+import MapView from 'react-native-maps';
 
-function OrderDelivery(props) {
+function OrderDelivery({route, navigation}) {
+  const [restaurant, setRestaurants] = React.useState(null);
+  const [streetName, setStreetName] = React.useState('');
+  const [fromLocation, setFromLocation] = React.useState(null);
+  const [toLocation, setToLocation] = React.useState(null);
+  const [region, setRegion] = React.useState(null);
+
+  React.useEffect(() => {
+    let {restaurant, currentLocation} = route.params;
+    let fromLoc = currentLocation.gps;
+    let toLoc = restaurant.location;
+    let street = currentLocation.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude + toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude + toLoc.longitude) * 2,
+    };
+    setRestaurants(restaurant);
+    setStreetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, [route.params]);
+  function renderMap() {
+    return (
+      <View style={{flex: 1}}>
+        <MapView style={{flex: 1}}></MapView>
+      </View>
+    );
+  }
+  function renderDistanceHeader() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: 50,
+          left: 0,
+          right: 0,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: SIZES.width * 0.9,
+            paddingHorizontal: SIZES.padding * 2,
+            paddingVertical: SIZES.padding,
+            borderRadius: SIZES.radius,
+          }}>
+          <TouchableOpacity
+            style={{
+              width: 50,
+              paddingRight: SIZES.padding * 2,
+              justifyContent: 'center',
+            }}
+            onPress={() => {
+              navigation.goBack();
+            }}>
+            <Image
+              source={icons.list}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+                justifyContent: 'center',
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>OrderDelivery</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      {renderMap()}
+      {/* {renderDistanceHeader()} */}
+    </SafeAreaView>
   );
 }
 
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray2,
+  },
+});
 export default OrderDelivery;
