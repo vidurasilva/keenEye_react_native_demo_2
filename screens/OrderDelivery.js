c5f0f512d10df4736a2540cd749dd52352c6685e
vidura_silva <vidura.silva@keeneye.solutions>
2022-07-20 11:17:56 +0530

    Set map and Direction in Order Delivery page

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import {
   View,
   SafeAreaView,
@@ -6,18 +7,32 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-import {COLORS, FONTS, SIZES, icons, images} from '../constants';
+import {
+  COLORS,
+  SIZES,
+  icons,
+  images,
+  GOOGLE_API_KEY,
+  FONTS,
+} from '../constants';
 import React from 'react';
-import MapView from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 function OrderDelivery({route, navigation}) {
+  const mapView = React.useRef();
   const [restaurant, setRestaurants] = React.useState(null);
   const [streetName, setStreetName] = React.useState('');
   const [fromLocation, setFromLocation] = React.useState(null);
   const [toLocation, setToLocation] = React.useState(null);
   const [region, setRegion] = React.useState(null);
 
+  const [duration, setDuration] = React.useState(0);
+  const [isReady, setIsReady] = React.useState(false);
+  const [angle, setAngle] = React.useState(0);
+
   React.useEffect(() => {
+    // eslint-disable-next-line no-shadow
     let {restaurant, currentLocation} = route.params;
     let fromLoc = currentLocation.gps;
     let toLoc = restaurant.location;
@@ -26,8 +41,8 @@ function OrderDelivery({route, navigation}) {
     let mapRegion = {
       latitude: (fromLoc.latitude + toLoc.latitude) / 2,
       longitude: (fromLoc.longitude + toLoc.longitude) / 2,
-      latitudeDelta: Math.abs(fromLoc.latitude + toLoc.latitude) * 2,
-      longitudeDelta: Math.abs(fromLoc.longitude + toLoc.longitude) * 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
     };
     setRestaurants(restaurant);
     setStreetName(street);
@@ -35,10 +50,99 @@ function OrderDelivery({route, navigation}) {
     setToLocation(toLoc);
     setRegion(mapRegion);
   }, [route.params]);
+
+  function calculateAngle(coordinates) {
+    let startLat = coordinates[0]['latitude'];
+    let startLng = coordinates[0]['longitude'];
+    let endLat = coordinates[1]['latitude'];
+    let endLng = coordinates[1]['longitude'];
+    let dx = endLat - startLat;
+    let dy = endLng - startLng;
+
+    return (Math.atan2(dy, dx) * 180) / Math.PI;
+  }
+
   function renderMap() {
+    const destinationMarker = () => (
+      <Marker coordinate={toLocation}>
+        <View
+          style={{
+            height: 40,
+            width: 40,
+            borderRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: COLORS.white,
+          }}>
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              borderRadius: 20,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.primary,
+            }}>
+            <Image
+              source={icons.pin}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
+            />
+          </View>
+        </View>
+      </Marker>
+    );
+    const carIcon = () => (
+      <Marker
+        coordinate={fromLocation}
+        anchor={{x: 0.5, y: 0.5}}
+        flat={true}
+        rotation={angle}>
+        <Image source={icons.car} style={{width: 40, height: 40}} />
+      </Marker>
+    );
+
     return (
       <View style={{flex: 1}}>
-        <MapView style={{flex: 1}}></MapView>
+        <MapView
+          ref={mapView}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={region}
+          style={{flex: 1}}>
+          <MapViewDirections
+            origin={fromLocation}
+            destination={toLocation}
+            apikey={GOOGLE_API_KEY}
+            strokewidth={5}
+            strokeColor={COLORS.primary}
+            optimizeWaypoints={true}
+            onReady={result => {
+              setDuration(result.duration);
+              if (!isReady) {
+                mapView.current.fitToCoordinates(result.coordinates, {
+                  edgePadding: {
+                    right: SIZES.width / 20,
+                    bottom: SIZES.height / 4,
+                    left: SIZES.width / 20,
+                    top: SIZES.height / 8,
+                  },
+                });
+                //car position
+                let nextLoc = {
+                  latitude: result.coordinates[0]['latitude'],
+                  longitude: result.coordinates[0]['longitude '],
+                };
+                if (result.coordinates.length >= 2) {
+                  let angle = calculateAngle(result.coordinates);
+                  setAngle(angle);
+                }
+                setFromLocation(nextLoc);
+                setIsReady(true);
+              }
+            }}
+          />
+          {destinationMarker()}
+          {carIcon()}
+        </MapView>
       </View>
     );
   }
@@ -62,42 +166,161 @@ function OrderDelivery({route, navigation}) {
             paddingHorizontal: SIZES.padding * 2,
             paddingVertical: SIZES.padding,
             borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
           }}>
-          <TouchableOpacity
+          <Image
+            source={icons.red_pin}
+            resizeMode="contain"
             style={{
-              width: 50,
-              paddingRight: SIZES.padding * 2,
+              width: 30,
+              height: 30,
+              marginRight: SIZES.padding,
               justifyContent: 'center',
             }}
-            onPress={() => {
-              navigation.goBack();
-            }}>
-            <Image
-              source={icons.list}
-              resizeMode="contain"
-              style={{
-                width: 30,
-                height: 30,
-                justifyContent: 'center',
-              }}
-            />
-          </TouchableOpacity>
+          />
+          <View style={{flex: 1}}>
+            <Text style={{...FONTS.body3}}>{streetName}</Text>
+          </View>
+          <Text>{Math.ceil(duration)} mins </Text>
         </View>
       </View>
     );
   }
+
+  function renderDeliveryInfo() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 50,
+          left: 0,
+          right: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding * 3,
+            paddingHorizontal: SIZES.padding * 2,
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.radius,
+          }}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Image
+              source={restaurant?.courier.avatar}
+              style={{width: 50, height: 50, borderRadius: 25}}
+            />
+            <View style={{flex: 1, marginLeft: SIZES.padding}}>
+              <View
+                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{...FONTS.h4}}>{restaurant?.courier.name}</Text>
+                <View style={{flexDirection: 'row'}}>
+                  <Image
+                    source={icons.star}
+                    style={{
+                      width: 18,
+                      height: 18,
+                      tintColor: COLORS.primary,
+                      marginRight: SIZES.padding,
+                    }}
+                  />
+                  <Text style={{...FONTS.body3}}>{restaurant?.rating}</Text>
+                </View>
+              </View>
+              <Text style={{color: COLORS.darkgray, ...FONTS.body4}}>
+                {restaurant?.name}
+              </Text>
+            </View>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginTop: SIZES.padding * 2,
+              justifyContent: 'space-between',
+            }}>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                height: 50,
+                marginRight: 10,
+                backgroundColor: COLORS.primary,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}
+              onPress={() => navigation.navigate('Home')}>
+              <Text style={{...FONTS.h4, color: COLORS.white}}>Call</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                height: 50,
+                backgroundColor: COLORS.secondary,
+                alignItems: 'center',
+                justifyContent: 'center',
+                borderRadius: 10,
+              }}
+              onPress={() => navigation.goBack()}>
+              <Text style={{...FONTS.h4, color: COLORS.white}}>Cancel</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  function renderButtons() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: SIZES.height * 0.35,
+          right: SIZES.padding * 2,
+          alignItems: 'center',
+        }}>
+        {/* zoom in */}
+        <TouchableOpacity
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: COLORS.white,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => zoomIn()}>
+          <Text style={{...FONTS.body1}}>+</Text>
+        </TouchableOpacity>
+
+        {/* zoom out */}
+        <TouchableOpacity
+          style={{
+            width: 60,
+            height: 60,
+            borderRadius: 30,
+            backgroundColor: COLORS.white,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => zoomOut()}>
+          <Text style={{...FONTS.body1}}>-</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
-    <SafeAreaView style={style.container}>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightGray2}}>
       {renderMap()}
-      {/* {renderDistanceHeader()} */}
+      {renderDistanceHeader()}
+      {renderDeliveryInfo()}
+      {renderButtons()}
     </SafeAreaView>
   );
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.lightGray2,
-  },
-});
 export default OrderDelivery;
